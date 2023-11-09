5a8b1b15525eb9d532e4a72ebbbd80bb02742fad
Redwi-v <forest45456@gmail.com>
2023-11-09 07:07:55 +0300

    cors2

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.enableCors({
-    credentials: false,
-    origin: false,
+    credentials: true,
+    origin: true,
   });
   await app.listen(process.env.APP_PORT || 4000);
 }
